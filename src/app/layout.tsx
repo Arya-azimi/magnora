a200e7550bd189e5fff755a2dc97282e6890f8de
Arya-azimi <aryaazimi200@gmail.com>
2026-06-13 17:52:58 +0330

    adding vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./styles/main.css";
 import { Header } from "@/components/header/header";
 import { Footer } from "@/components/footer/footer";
 import SmoothScroll from "@/components/SmoothScroll";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Magnora - AI ",
@@ -18,11 +19,33 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
-        <Header/>
-        <SmoothScroll>
-          {children}
-        </SmoothScroll>
-        <Footer/>
+        <Header />
+        <SmoothScroll>{children}</SmoothScroll>
+        <Footer />
+        <Analytics
+          beforeSend={(event) => {
+            const hostname = window.location.hostname;
+
+            if (hostname.includes("-git-") || hostname.includes("localhost")) {
+              return null;
+            }
+
+            const url = new URL(event.url);
+            if (
+              url.searchParams.has("fbclid") ||
+              url.searchParams.has("gclid")
+            ) {
+              url.searchParams.delete("fbclid");
+              url.searchParams.delete("gclid");
+              return {
+                ...event,
+                url: url.toString(),
+              };
+            }
+
+            return event;
+          }}
+        />
       </body>
     </html>
   );
